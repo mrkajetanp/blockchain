02bd3306dcef61bee1d667d8f5afa5cc3d96ebf8
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2019-05-28 23:48:43 +0200

    [Solidity] Basics - Receiver pays 2/2

--- Ethereum/Solidity/Docs/receiver_pays.sol
+++ Ethereum/Solidity/Docs/receiver_pays.sol
@@ -17,4 +17,32 @@ contract ReceiverPays {
         require(recoverSigner(message, signature) == owner);
         msg.sender.transfer(amount);
     }
+
+    // destroy and reclaim leftover funds
+    function kill() public {
+        require(msg.sender == owner);
+        selfdestruct(msg.sender);
+    }
+
+    function splitSignature(bytes memory sig) internal pure returns (uint8 v, bytes32 r, bytes32 s) {
+        require(sig.length == 65);
+
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := byte(0, mload(add(sig, 96)))
+        }
+
+        return (v, r, s);
+    }
+
+    function recoverSigner(bytes32 message, bytes memory sig) internal pure returns (address) {
+        (uint8 v, bytes32 r, bytes32 s) = splitSignature(sig);
+        return ecrecover(message, v, r, s);
+    }
+
+    // prefix hash to mimic the behaviour of eth_sign
+    function prefixed(bytes32 hash) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked("\x19Ethereum signed message:\n32", hash));
+    }
 }
