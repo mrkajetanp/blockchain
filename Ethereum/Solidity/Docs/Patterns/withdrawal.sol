dbff0e6f1749998aa2864b1f46d22d2506123d92
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2019-06-05 12:40:08 +0200

    [Solidity] Common Patterns

---
+++ Ethereum/Solidity/Docs/Patterns/withdrawal.sol
@@ -0,0 +1,31 @@
+pragma solidity >=0.5.0 <0.7.0;
+
+contract WithdrawalContract {
+    address public richest;
+    uint public mostSent;
+
+    mapping(address => uint) pendingWithdrawals;
+
+    constructor() public payable {
+        richest = msg.sender;
+        mostSent = msg.value;
+    }
+
+    function becomeRichest() public payable returns (bool) {
+        if (msg.value > mostSent) {
+            pendingWithdrawals[richest] += msg.value;
+            richest = msg.sender;
+            mostSent = msg.value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function withdraw() public {
+        uint amount = pendingWithdrawals[msg.sender];
+        // Remember to zero the pending refund to prevent re-entrancy attacks
+        pendingWithdrawals[msg.sender] = 0;
+        msg.sender.transfer(amount);
+    }
+}
