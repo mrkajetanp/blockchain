fe286063dcf445a106967ed4c5f917750a980bf7
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2019-07-13 20:41:35 +0200

    [Solidity] Custom ERC20 token

--- Ethereum/Solidity/Patterns/erc20Token.sol
+++ Ethereum/Solidity/Patterns/erc20Token.sol
@@ -2,6 +2,63 @@ pragma solidity ^0.4.25;
 
 import "./erc20.sol";
 
+library SafeMath {
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+      assert(b <= a);
+      return a — b;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256)   {
+      uint256 c = a + b;
+      assert(c >= a);
+      return c;
+    }
+}
+
 contract ExampleToken is ERC20Interface {
-    
+    mapping(address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+
+    uint256 totalSupply_;
+
+    constructor(uint256 total) public {
+        totalSupply_ = total;
+        balances[msg.sender] = _totalSupply;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return totalSupply_;
+    }
+
+    function balanceOf(address tokenOwner) public view returns (uint) {
+        return balances[tokenOwner];
+    }
+
+    function transfer(address receiver, uint numTokens) public returns (bool) {
+        require(numTokens <= balances[msg.sender]);
+        balances[msg.sender] = balances[msg.sender].sub(numTokens);
+        balances[receiver] = balances[receiver].add(numTokens);
+        emit Transfer(msg.sender, receiver, numTokens);
+        return true;
+    }
+
+    function approve(address delegate, uint numTokens) public returns (bool) {
+        allowed[msg.sender][delegate] = numTokens;
+        emit Approval(msg.sender, delegate, numTokens);
+        return true;
+    }
+
+    function allowance(address owner, address delegate) public view returns (uint) {
+        return allowed[owner][delegate];
+    }
+
+    function transferFrom(address owner, address buyer, uint numTokens) public returns (bool) {
+        require(numTokens <= balances[owner]);
+        require(numTokens <= allowed[owner][msg.sender]);
+        balances[owner] = balances[owner].sub(numTokens);
+        allowed[owner][msg.sender] = allowed[from][msg.sender].sub(numTokens);
+        balances[buyer] = balances[buyer].add(numTokens);
+        emit Transfer(owner, buyer, numTokens);
+        return true;
+    }
 }
