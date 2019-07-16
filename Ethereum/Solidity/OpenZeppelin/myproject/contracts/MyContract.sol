23408a9e6dc1f7ad63de0ad1948f77d5fe8b9cbf
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2019-07-16 12:55:31 +0200

    [Solidity] OpenZeppelin - Deeper Basics

--- Ethereum/Solidity/OpenZeppelin/myproject/contracts/MyContract.sol
+++ Ethereum/Solidity/OpenZeppelin/myproject/contracts/MyContract.sol
@@ -1,7 +1,20 @@
 pragma solidity ^0.5.0;
 
 import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
+import "openzeppelin-solidity/contracts/token/ERC777/ERC777.sol";
 
 contract MyContract is Ownable {
 
+}
+
+contract GLDToken is ERC777 {
+    constructor(
+        uint256 initialSupply,
+        address[] memory defaultOperators
+    )
+        ERC777("Gold", "GLD", defaultOperators)
+        public
+    {
+        _mint(msg.sender, msg.sender, initialSupply, "", "");
+    }
 }
